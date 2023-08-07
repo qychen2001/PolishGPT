7994d1e27172292fc6fd5504f5e55f46bb43a5d0
qiyuanchen <chenqiyuan1012@foxmail.com>
2023-08-07 16:54:12 +0800

    polish is done

---
+++ model.py
@@ -0,0 +1,123 @@
+from langchain.llms import OpenAI
+from langchain.prompts import Prompt
+import openai
+
+import gradio as gr
+
+# llm = OpenAI(openai_api_key="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")
+#
+# print(llm("tell me a joke"))
+
+FEACTURE_DICT = {
+    "更精确的措辞": "More precise: Use more precise terms, such as 'generate' instead of 'produce' or 'analyze' instead of 'look at'.",
+    "更简练的表达": "More concise: Eliminate unnecessary words and phrases to make the sentences clearer and more direct.",
+    "更客观的语言": "More objective: Present information in a neutral manner by removing subjective language.",
+    "更具体的描述": "More specific: Provide more specific details to support arguments or ideas.",
+    "更连贯的表达": "More coherent: Ensure well-organized and logically flowing sentences.",
+    "更一致的风格": "More consistent: Maintain consistency in language and style with the rest of the paper.",
+    "更符合学术风格": "More academic: Use commonly used academic terms and phrases, such as 'furthermore' and 'thus'.",
+    "更正式的语法": "More formal grammar: Utilize correct grammar and syntax, avoiding sentence fragments or off-topic sentences.",
+    "更具细节的描述": "More nuanced: Convey more complex or subtle meanings by using specific words or phrases, making the sentences more nuanced."
+}
+
+LEVEL_DICT = {
+    "仅对文本进行微调": "Subtle edits only",
+    "进行一些小的编辑": "Minor edits",
+    "改写以提高表达清晰度": "Rephrase for clarity",
+    "简化句子结构": "Simplify sentence structure",
+    "检查语法和拼写": "Check grammar and spelling",
+    "提高文本流畅度和连贯性": "Enhance flow and coherence",
+    "改善用词": "Improve word choice",
+    "为文本调整风格": "Revise for style",
+    "进行重大编辑": "Significant edits",
+    "重新构建内容": "Restructure content"
+}
+
+TYPE_DICT = {
+    "会议": "conference",
+    "期刊": "journal"
+}
+
+DOMAIN_DICT = {
+    "人工智能": "artificial intelligence",
+    "计算机视觉": "computer vision",
+    "自然语言处理": "natural language processing",
+    "机器学习": "machine learning"
+}
+
+
+def format_proxy(proxy):
+    if proxy is None:
+        return None
+    else:
+        return "http://" + proxy.replace("http://", "").replace("https://", "")
+
+
+def ask_gpt(prompt, api_key, proxy, model_name="gpt-3.5-turbo", temperature=0):
+    openai.api_key = api_key
+    openai.proxy = proxy
+    try:
+        response = openai.ChatCompletion.create(
+            model=model_name,
+            messages=[{"role": "user", "content": prompt}],
+            temperature=temperature,
+        ).choices[0].message['content']
+        return response
+    except Exception as e:
+        openai.api_key = None
+        openai.proxy = None
+        return f"请求时发生错误，请重试！ {str(e)}"
+
+
+def check_available(api_key, proxy=None, model_name="gpt-3.5-turbo"):
+    openai.api_key = api_key
+    openai.proxy = proxy
+    try:
+        openai.ChatCompletion.create(
+            model=model_name,
+            messages=[{"role": "user", "content": "Hello!"}],
+            temperature=0,
+        )
+    except Exception as e:
+        openai.api_key = None
+        openai.proxy = None
+        return f"检查可用性时发生了错误: {str(e)}"
+    return "模型可用！"
+
+
+def polish_prompt(polish_level, features_p, length_req, length, input_text, paper_type, paper_domain):
+    if length_req == "扩写":
+        length_prompt = f"Expand the original content to {length} words."
+    elif length_req == "缩写":
+        length_prompt = f"Condense the original content to {length} words."
+    else:
+        length_prompt = "Keep the original length."
+
+    features_prompt = ""
+    if len(features_p) != 0:
+        # 把feature_p中的所有内容添加到一个字符串
+        features_prompt = "\n".join(FEACTURE_DICT[key] for key in features_p)
+
+    prompt = f"""Assume you are an expert in the {DOMAIN_DICT[paper_domain]}, and you excel at writing {TYPE_DICT[paper_type]} papers in this field. 
+You are exceptionally skilled at polishing a piece of text to meet publication standards.
+Your task is to refine the input text to meet publication standards based on the requirements.
+The input text is enclosed in triple single quotation marks ('''), for example: '''This is the input text.'''
+For the input text, you need to {LEVEL_DICT[polish_level]}. And you need to {length_prompt}.
+The more detailed requirements are as follows:
+{features_prompt}
+
+The input text is:
+'''
+{input_text}
+''' 
+The polished text should be enclosed in <<<>>>. For example: <<<This is the polished text.>>>    
+"""
+    return prompt
+
+
+def polish(polish_level, features_p, length_req, length, input_text, paper_type, paper_domain, api_key, proxy=None,
+           model_name="gpt-3.5-turbo", temperature=0):
+    prompt = polish_prompt(polish_level, features_p, length_req, length, input_text, paper_type, paper_domain)
+    return ask_gpt(prompt, api_key, proxy, model_name, temperature).strip("<<<").strip(">>>")
+
+
--- webui.py
+++ webui.py
@@ -1,40 +1,73 @@
 import gradio as gr
+from model import check_available, polish, ask_gpt
 
 with gr.Blocks() as interface:
+    gr.Markdown(
+        value="""
+        # PolishGPT: 用GPT改善你的论文写作！
+        由于每次都要用ChatGPT润色都要写复杂的Prompt，所以我设计了一个WebUI，可以快速润色论文。
+        如果这个项目对你有帮助，欢迎去[这里](https://github.com/qiyuan-chen/PolishGPT)给我点一个star！有任何问题或者你觉得好用的润色Prompt也欢迎在Issue中提出。
+        ## **这个项目由[陈启源](https://qiyuan-chen.github.io/) @ 华中师范大学 开发。目前陈启源同学正在寻找24Fall推免导师，欢迎联系！**
+        """)
     with gr.Row():
-        with gr.Tab("基础模型设置"):
-            api_key = gr.Textbox(label="OpenAI API Key")
-            proxy = gr.Textbox(label="代理地址，在国内使用需要设置")
-            model_name = gr.Dropdown(choices=["gpt-3.5-turbo", "gpt-4"], label="使用的模型")
-            temperature = gr.Slider(minimum=0.0, maximum=1.0, value=0, step=0.1, label="Temperature",
-                                    info="控制生成文本的随机性。数值越小，模型的确定性越高")
-        with gr.Tab("基础论文设置"):
-            paper_type = gr.Dropdown(choices=["会议", "期刊"], label="论文类型")
-            paper_domain = gr.Dropdown(choices=['人工智能', '自然语言处理', '计算机视觉', '机器学习'], label="论文领域")
+        with gr.Tab("基础设置"):
+            with gr.Row():
+                with gr.Column():
+                    api_key = gr.Textbox(label="OpenAI API Key")
+                    temperature = gr.Slider(minimum=0.0, maximum=1.0, value=0, step=0.1, label="Temperature",
+                                            info="控制生成文本的随机性。数值越小，模型的确定性越高")
+                with gr.Column():
+                    model_name = gr.Dropdown(choices=["gpt-3.5-turbo", "gpt-4"], label="使用的模型")
+                    check = gr.Button(value="检查可用性")
+                with gr.Column():
+                    proxy = gr.Textbox(label="代理地址，在国内使用需要设置")
+                    check_status = gr.Textbox(label="检查结果")
+                check.click(check_available, inputs=[api_key, proxy, model_name], outputs=[check_status])
+                with gr.Column():
+                    paper_type = gr.Dropdown(choices=["会议", "期刊"], label="论文类型")
+                    paper_domain = gr.Dropdown(choices=['人工智能', '自然语言处理', '计算机视觉', '机器学习'],
+                                               label="论文领域")
 
         # with gr.Tab("语言风格设置"):
 
     with gr.Row():
         with gr.Tab("润色"):
             # 润色程度
-            polish_level = gr.Dropdown(choices=[])
-            input_text = gr.Textbox(lines=6, label="润色前的文本")
-            output_text = gr.Textbox(lines=6, label="润色后的文本", show_copy_button=True)
-            submit = gr.Button(label="开始润色")
+            polish_level = gr.Dropdown(
+                choices=["仅对文本进行微调", "进行一些小的编辑", "改写以提高表达清晰度", "简化句子结构",
+                         "检查语法和拼写", "提高文本流畅度和连贯性", "改善用词", "为文本调整风格", "进行重大编辑",
+                         "重新构建内容"], label="润色程度/功能")
+            features_p = gr.Checkboxgroup(
+                choices=["更精确的措辞", "更简练的表达", "更客观的语言", "更具体的描述", "更连贯的表达", "更一致的风格",
+                         "更符合学术风格", "更正式的语法", "更具细节的描述"], label="具体要求",
+                info="此选项不建议一次选太多！")
+            length_req = gr.Dropdown(choices=["保持原长度", "扩写", "缩写"], label="文本长度要求")
+            length = gr.Slider(minimum=20, maximum=2000, step=10, label="生成文本长度",
+                               info="控制润色后的文本长度，当文本长度要求设置为保持原长度时，这一选项不起作用")
+            input_text_1 = gr.Textbox(lines=6, label="润色前的文本")
+            output_text_1 = gr.Textbox(lines=6, label="润色后的文本", show_copy_button=True)
+            submit_1 = gr.Button(value="开始润色")
+
+            submit_1.click(fn=polish,
+                           inputs=[polish_level, features_p, length_req, length, input_text_1, paper_type, paper_domain,
+                                   api_key, proxy, model_name, temperature], outputs=[output_text_1])
 
         with gr.Tab("翻译"):
+            features_t = gr.Checkboxgroup(
+                choices=["更精确的措辞", "更简练的表达", "更客观的语言", "更具体的描述", "更连贯的表达", "更一致的风格",
+                         "更符合学术风格", "更正式的语法", "更具细节的描述"])
             with gr.Row():
                 with gr.Column():
-                    source_language = gr.Dropdown(choices=["中文", "英文"], label="源语言")
+                    source_language = gr.Dropdown(choices=["中文", "英文"], label="源语言", value="英文")
                 with gr.Column():
-                    target_language = gr.Dropdown(choices=["中文", "英文"], label="目标语言")
-            input_text = gr.Textbox(lines=6, label="翻译前的文本")
-            output_text = gr.Textbox(lines=6, label="翻译后的文本", show_copy_button=True)
-            submit = gr.Button(label="开始翻译")
+                    target_language = gr.Dropdown(choices=["中文", "英文"], label="目标语言", value="中文")
+            input_text_2 = gr.Textbox(lines=6, label="翻译前的文本")
+            output_text_2 = gr.Textbox(lines=6, label="翻译后的文本", show_copy_button=True)
+            submit_2 = gr.Button(label="开始翻译", )
 
         with gr.Tab("要点生成段落"):
-            input_text = gr.Textbox(lines=6, label="输入要点")
-            output_text = gr.Textbox(lines=6, label="输出文本", show_copy_button=True)
-            submit = gr.Button(label="开始生成")
+            input_text_3 = gr.Textbox(lines=6, label="输入要点")
+            output_text_3 = gr.Textbox(lines=6, label="输出文本", show_copy_button=True)
+            submit_3 = gr.Button(label="开始生成")
 
-interface.launch()
+interface.launch(share=True)
